a82d712e75d6e2921c773464f4dd4020a4aac3b2
Victor Wang <victorwang2001@gmail.com>
2020-04-23 21:00:10 -0400

    finished adding story

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -1,21 +1,25 @@
 import React from 'react';
+import "./AboutUs.css";
 
 const AboutUsPage = props => {
    return(
       <div className="container">
+         <div className="row" id="aboutUsTitle">
+         Who are we?
+         </div>
          <div className="row">
-            <div>
-               <h1 id="aboutUsTitle"> Who are we? </h1>
-               <p id="aboutUsText"> testing testing 123 </p>
+            <div className="card" id="aboutUsBody">
+               <div className="card-body">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+               </div>
             </div>
          </div>
-        
-         <div className="row">
-            <div className="col">
+         <div className="row" id="info">
+            <div>
                <h3> V </h3>
                <p> personal info stuff </p>
             </div>
-            <div className="col">
+            <div>
                <h3> S </h3>
                <p> more personal info stuff </p>
             </div>
--- client/src/components/addStory/AddStory.js
+++ client/src/components/addStory/AddStory.js
@@ -1,31 +1,58 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, {useState} from 'react';
+import {connect} from 'react-redux';
+import {addPost} from '../../actions/mypostActions';
 
 import './AddStory.css';
 
 const AddStory = props => {
-   function sendData(e){
+   const [postTitle, setPostTitle] = useState("");
+   const [postArticle, setPostArticle] = useState("");
 
+   function sendData(e){
+      if(postTitle != "" && postArticle != ""){
+         const newPost = {
+            title: postTitle,
+            body: postArticle
+         } 
+
+         props.addPost(newPost); 
+      }
    }
 
 
    return(
-      <form>
-         <div className="form-group">
-            <label for="title" className="inputLabel"> Title </label>
-            <input type="text" name="title" id="title" class="form-control" />
+      <form id="input-form">
+         <div className="form-group"> 
+
+            <div id="first-row">
+               <label htmlFor="title" className="inputLabel"> Title </label>
+
+               <div className="dropdown" id="catagory-dropdown">
+                 <a className="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                   Catagory
+                 </a>
+
+                 <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                   <a className="dropdown-item" href="#">some catagory</a>
+                   <a className="dropdown-item" href="#">some more catagory</a>
+                   <a className="dropdown-item" href="#">Urgent</a>
+                 </div>
+               </div>
+            </div>
+
+            <input type="text" name="title" id="postTitle" className="form-control" onChange={(e) => setPostTitle(e.target.value)} />
          </div>
 
          <div className="form-group">
-            <label for="article" className="inputLabel"> Story </label>
-            <textarea type="text" id="article" name="article" class="form-control" rows="3" />
+            <label htmlFor="article" className="inputLabel"> Story </label>
+            <textarea type="text" id="postArticle" name="article" className="form-control" rows="3" onChange={(e) => setPostArticle(e.target.value)} />
          </div>
 
-         <button type="button" className="btn btn-primary"> 
-            Post
+         <button type="button" className="btn btn-primary" id="submit-btn" onClick={sendData}> 
+            +
          </button>
       </form>
    );
 }
 
-export default AddStory;
+export default connect(null, {addPost})(AddStory);
